0b715e968e50ec15e0ca96884084b102fdce36a7
Siddharth VP <siddharthvp@gmail.com>
2021-12-25 01:29:57 +0530

    Add getSkins() in GadgetResourceLoaderModule
    Overrides ResourceLoaderModule#getSkins() as introduced in core
    with I08a1a59b4eab90d380ca8f874, so that skin-specific gadgets are
    only registered client-side if applicable to the current skin.
    
    Bug: T236603
    Depends-On: I08a1a59b4eab90d380ca8f874bb6dbba2f399590
    Change-Id: I111a67c3119a294afc093b7b64bf0927a56f5b1f

--- includes/GadgetResourceLoaderModule.php
+++ includes/GadgetResourceLoaderModule.php
@@ -108,6 +108,10 @@ class GadgetResourceLoaderModule extends ResourceLoaderWikiModule {
 		return $this->getGadget()->getTargets();
 	}
 
+	public function getSkins(): ?array {
+		return $this->getGadget()->getRequiredSkins() ?: null;
+	}
+
 	public function getGroup() {
 		return 'site';
 	}
